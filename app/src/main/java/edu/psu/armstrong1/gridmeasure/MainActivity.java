f2a6d81f7022f2b84e29adae851773c386a0b06f
Gus Smith <gushenrysmith@gmail.com>
2017-03-20 21:47:45 -0400

    Cleans up file and removes some old unused code.

--- app/src/main/java/edu/psu/armstrong1/gridmeasure/MainActivity.java
+++ app/src/main/java/edu/psu/armstrong1/gridmeasure/MainActivity.java
@@ -9,8 +9,10 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    // Load OpenCV.
-    static {System.loadLibrary("opencv_java3");}
+    static {
+        System.loadLibrary("opencv_java3");
+        System.loadLibrary("native-lib");
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,18 +21,6 @@ public class MainActivity extends AppCompatActivity {
 
         // Example of a call to a native method
         TextView tv = (TextView) findViewById(R.id.sample_text);
-        tv.setText(stringFromJNI());
-    }
-
-    /**
-     * A native method that is implemented by the 'native-lib' native library,
-     * which is packaged with this application.
-     */
-    public native String stringFromJNI();
-
-    // Used to load the 'native-lib' library on application startup.
-    static {
-        System.loadLibrary("native-lib");
     }
 
     // Called when the user clicks the take picture button
