e4bc5be8af4f940587c9fd4bfdbafcf832fcf7cc
Gus Smith <gushenrysmith@gmail.com>
2017-04-08 13:54:00 -0400

    puts logic for finding charuco boardss into its own function

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -5,6 +5,34 @@
 #include "opencv2/aruco/dictionary.hpp"
 #include "opencv2/imgproc.hpp"
 
+bool findCharuco(
+        cv::Mat in,
+        const cv::Ptr<cv::aruco::CharucoBoard> board,
+        std::vector<cv::Point2f>& charucoCorners,
+        std::vector<int>& charucoIds)
+{
+    std::vector< int > markerIds;
+    std::vector< std::vector<cv::Point2f> > markerCorners;
+    cv::aruco::detectMarkers(in, board->dictionary, markerCorners, markerIds);
+
+    // if at least one marker detected
+    if(markerIds.size() > 0) {
+        cv::aruco::interpolateCornersCharuco(markerCorners, markerIds, in, board, charucoCorners, charucoIds);
+        return true;
+    }
+    return false;
+}
+
+
+extern "C"
+void Java_edu_psu_armstrong1_gridmeasure_GridDetectionUtils_calibrateWithCharuco(
+    JNIEnv* env,
+    jobject,
+    jobjectArray imagePathsArray)
+{
+
+}
+
 extern "C"
 jstring
 Java_edu_psu_armstrong1_gridmeasure_GridDetectionUtils_stringFromJNI(
@@ -28,18 +56,10 @@ Java_edu_psu_armstrong1_gridmeasure_GridDetectionUtils_stringFromJNI(
     const cv::Ptr<cv::aruco::Dictionary> dictionary = cv::aruco::getPredefinedDictionary(cv::aruco::DICT_7X7_1000);
     const cv::Ptr<cv::aruco::CharucoBoard> board = cv::aruco::CharucoBoard::create(5, 7, 100, 50, dictionary);
 
-    std::vector< int > markerIds;
-    std::vector< std::vector<cv::Point2f> > markerCorners;
-    cv::aruco::detectMarkers(*pInMat, board->dictionary, markerCorners, markerIds);
-
-    // if at least one marker detected
-    if(markerIds.size() > 0) {
-        std::vector<cv::Point2f> charucoCorners;
-        std::vector<int> charucoIds;
-        cv::aruco::interpolateCornersCharuco(markerCorners, markerIds, *pInMat, board, charucoCorners, charucoIds);
-
+    std::vector<cv::Point2f> charucoCorners;
+    std::vector<int> charucoIds;
+    if (findCharuco(*pInMat, board, charucoCorners, charucoIds)) {
         cv::aruco::drawDetectedCornersCharuco(*pOutMat, charucoCorners, charucoIds, cv::Scalar(255,255,255));
-
     }
 
     std::string hello = "hello";
